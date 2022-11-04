7da2f4698706eaefe3725b677cb494763f6461c2
Thistle <lisamariejonasson@gmail.com>
2022-11-03 20:02:14 -0400

    Re-add sprite turning animations

--- Assets/CharacterControllerMovement.cs
+++ Assets/CharacterControllerMovement.cs
@@ -6,10 +6,20 @@ public class CharacterControllerMovement : MonoBehaviour
 {
     private Vector3 PlayerMovementInput;
     private Vector3 Velocity;
-    private float xRot;
+
+    private SpriteRenderer PlayerSprite;
 
     [SerializeField] private CharacterController Controller;
     [SerializeField] private float Speed;
+
+    [SerializeField] private Sprite frontSprite;
+    [SerializeField] private Sprite backSprite;
+    [SerializeField] private Sprite sideSprite;
+
+    void Start()
+    {
+        PlayerSprite = GetComponentInChildren<SpriteRenderer>();    
+    }
 
     void Update()
     {
@@ -22,5 +32,26 @@ public class CharacterControllerMovement : MonoBehaviour
     {
         Vector3 moveVector = transform.TransformDirection(PlayerMovementInput);
         Controller.Move(moveVector * Speed * Time.deltaTime);
+
+        if(PlayerMovementInput.x < -0.3)
+        {
+            PlayerSprite.sprite = sideSprite;
+            PlayerSprite.flipX = true;
+        }
+        if(PlayerMovementInput.x > 0.3)
+        {
+            PlayerSprite.sprite = sideSprite;
+            PlayerSprite.flipX = false;
+        }
+        if(PlayerMovementInput.z < -0.1)
+        {
+            PlayerSprite.sprite = frontSprite;
+            PlayerSprite.flipX = false;
+        }
+        if(PlayerMovementInput.z > 0.1)
+        {
+            PlayerSprite.sprite = backSprite;
+            PlayerSprite.flipX = false;
+        }
     }
 }
