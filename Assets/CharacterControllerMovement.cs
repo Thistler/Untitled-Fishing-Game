9bafda1a142398af6f98adb916bae61b382661e6
Thistle <lisamariejonasson@gmail.com>
2022-11-04 02:37:51 -0400

    Cast bobber based on player direction

--- Assets/CharacterControllerMovement.cs
+++ Assets/CharacterControllerMovement.cs
@@ -9,12 +9,19 @@ public class CharacterControllerMovement : MonoBehaviour
 
     private SpriteRenderer PlayerSprite;
 
+    private bool Fishing;
+
     [SerializeField] private CharacterController Controller;
     [SerializeField] private float Speed;
 
     [SerializeField] private Sprite frontSprite;
     [SerializeField] private Sprite backSprite;
     [SerializeField] private Sprite sideSprite;
+
+    [SerializeField] private Transform BobberStartPoint;
+    [SerializeField] private GameObject Bobber;
+
+    private GameObject newBobber;
 
     void Start()
     {
@@ -25,7 +32,45 @@ public class CharacterControllerMovement : MonoBehaviour
     {
         PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
 
-        MovePlayer();
+        if(!Fishing)
+        {
+            MovePlayer();
+
+            if(Input.GetKey(KeyCode.Mouse0))
+            {
+                Fishing = true;
+
+                // Get our direction
+                if (PlayerSprite.sprite == frontSprite)
+                {
+                    BobberStartPoint.eulerAngles = new Vector3(0f, 180f, 0f);
+                }
+                else if (PlayerSprite.sprite == backSprite)
+                {
+                    BobberStartPoint.eulerAngles = new Vector3(0f, 0f, 0f);
+                }
+                else if (PlayerSprite.sprite == sideSprite && PlayerSprite.flipX)
+                {
+                    BobberStartPoint.eulerAngles = new Vector3(0f, 270f, 0f);
+                }
+                else if (PlayerSprite.sprite == sideSprite && !PlayerSprite.flipX) {
+                    BobberStartPoint.eulerAngles = new Vector3(0f, 90f, 0f);
+                }
+
+                newBobber = (GameObject)Instantiate(Bobber, BobberStartPoint.position, new Quaternion(0f, 0f, 0f, 0f));
+                newBobber.GetComponent<Rigidbody>().AddForce(BobberStartPoint.forward * 700f);
+            }
+        }
+        if (Fishing)
+        {
+            // Hit Q to cancel cast
+            // TODO: This should not be doable when a fish is on the line
+            if(Input.GetKey(KeyCode.Q))
+            {
+                Fishing = false;
+                if (newBobber) Destroy(newBobber, 0);
+            }
+        }
     }
 
     private void MovePlayer()
@@ -33,24 +78,24 @@ public class CharacterControllerMovement : MonoBehaviour
         Vector3 moveVector = transform.TransformDirection(PlayerMovementInput);
         Controller.Move(moveVector * Speed * Time.deltaTime);
 
-        if(PlayerMovementInput.x < -0.3)
-        {
-            PlayerSprite.sprite = sideSprite;
-            PlayerSprite.flipX = true;
-        }
-        if(PlayerMovementInput.x > 0.3)
-        {
-            PlayerSprite.sprite = sideSprite;
-            PlayerSprite.flipX = false;
-        }
-        if(PlayerMovementInput.z < -0.1)
+        if (PlayerMovementInput.z < -0.1)
         {
             PlayerSprite.sprite = frontSprite;
             PlayerSprite.flipX = false;
         }
-        if(PlayerMovementInput.z > 0.1)
+        if (PlayerMovementInput.z > 0.1)
         {
             PlayerSprite.sprite = backSprite;
+            PlayerSprite.flipX = false;
+        }
+        if (PlayerMovementInput.x < -0.3)
+        {
+            PlayerSprite.sprite = sideSprite;
+            PlayerSprite.flipX = true;
+        }
+        if (PlayerMovementInput.x > 0.3)
+        {
+            PlayerSprite.sprite = sideSprite;
             PlayerSprite.flipX = false;
         }
     }
